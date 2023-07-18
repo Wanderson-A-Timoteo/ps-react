acd8f33cfe640d3d1ece32545efe39a8c43d0a79
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-07-18 19:32:23 -0400

    feat: Added page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,80 @@
+import { Main, Container, Superior, Centro, DataInicio, DataFim, Label, Input, NomeOperador, ButtomPesquisar, TableContainer, Table, Caption, Thead, Thow, Tbody, TableHeader, TableData, TableFooter, PaginationButton } from './style';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Olá mundo!</h1>
-    </main>
-  )
+    <Main>
+      <Container>
+        <Superior>
+          <DataInicio>
+            <Label>Data de Início:</Label>
+            <Input />
+          </DataInicio>
+          <DataFim>
+            <Label>Data de Fim:</Label>
+            <Input />
+          </DataFim>
+          <NomeOperador>
+            <Label>Nome do Operador Transacionado:</Label>
+            <Input />
+          </NomeOperador>
+        </Superior>
+        <Centro>
+          <ButtomPesquisar>Pesquisar</ButtomPesquisar>
+        </Centro>
+
+        <TableContainer>
+          <Table>
+            <Caption> Saldo Total:    Saldo no Período:  </Caption>
+              <Thead>
+                <Thow>
+                  <TableHeader>Dados</TableHeader>
+                  <TableHeader>Valencia</TableHeader>
+                  <TableHeader>Tipo</TableHeader>
+                  <TableHeader>Nome do Operador Transacionado</TableHeader>
+                </Thow>
+              </Thead>
+              <Tbody>             
+
+                <Thow>
+                  <TableData>dataTransferencia</TableData>
+                  <TableData>valor</TableData>
+                  <TableData>tipo</TableData>
+                  <TableData>nomeOperadorTransacao</TableData>
+                </Thow>
+
+                <Thow>
+                  <TableData>dataTransferencia</TableData>
+                  <TableData>valor</TableData>
+                  <TableData>tipo</TableData>
+                  <TableData>nomeOperadorTransacao</TableData>
+                </Thow>
+
+                <Thow>
+                  <TableData>dataTransferencia</TableData>
+                  <TableData>valor</TableData>
+                  <TableData>tipo</TableData>
+                  <TableData>nomeOperadorTransacao</TableData>
+                </Thow>
+
+                <Thow>
+                  <TableData>dataTransferencia</TableData>
+                  <TableData>valor</TableData>
+                  <TableData>tipo</TableData>
+                  <TableData>nomeOperadorTransacao</TableData>
+                </Thow>
+
+              </Tbody>
+              <TableFooter>
+                <Thow>
+                  <TableData colSpan={4}>
+                    <PaginationButton>Anterior</PaginationButton>
+                    <PaginationButton>Próxima</PaginationButton>
+                  </TableData>
+                </Thow>
+              </TableFooter>
+          </Table>
+        </TableContainer>
+      </Container>
+    </Main>
+  );
 }
---
+++ src/app/style.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+
+`;
+
+export const Container = styled.div`
+
+`;
+
+export const Superior = styled.div`
+
+`;
+
+export const Centro = styled.div`
+
+`;
+
+export const DataInicio = styled.div`
+
+`;
+
+export const DataFim = styled.div`
+
+`;
+
+export const Label = styled.div`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const NomeOperador = styled.div`
+
+`;
+
+export const ButtomPesquisar = styled.button`
+
+`;
+
+export const TableContainer = styled.div`
+`;
+
+export const Table = styled.table`
+
+`;
+
+export const Caption = styled.caption`
+
+`;
+
+export const Thead = styled.thead`
+
+`;
+
+export const Thow = styled.tr`
+
+`;
+
+export const Tbody = styled.tbody`
+
+`;
+
+export const TableHeader = styled.th`
+
+`;
+
+export const TableData = styled.td`
+
+`;
+
+export const TableFooter = styled.tfoot`
+
+`;
+
+export const PaginationButton = styled.button`
+
+`;
