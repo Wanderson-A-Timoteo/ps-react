1b240f96824a9d98a874f9474faba61be82c1782
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-07-19 08:48:16 -0400

    feat: Added styling and pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,118 @@
-import { Main, Container, Superior, Centro, DataInicio, DataFim, Label, Input, NomeOperador, ButtomPesquisar, TableContainer, Table, Caption, Thead, Thow, Tbody, TableHeader, TableData, TableFooter, PaginationButton } from './style';
+'use client'
+
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import axios from 'axios';
+import { Main, Container, Superior, Centro, DataInicio, DataFim, Label, Input, NomeOperador, ButtomPesquisar, TableContainer, Caption, TableHeader, TableData, TableFooter, StyledReactPaginate } from './style';
 
 export default function Home() {
+  const [transferencias, setTransferencias] = useState([]);
+  const [saldoTotal, setSaldoTotal] = useState(0);
+  const [saldoTotalNoPeriodo, setSaldoTotalNoPeriodo] = useState(0);
+  const [dataInicio, setDataInicio] = useState('');
+  const [dataFim, setDataFim] = useState('');
+  const [nomeOperador, setNomeOperador] = useState('');
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 4;
+  const pageCount = Math.ceil(transferencias.length / itemsPerPage);
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+  };
+
+  const displayedTransferencias = transferencias.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
+
+  const handleSearch = async () => {
+    try {
+      const formattedDataInicio = formatDateTime(dataInicio);
+      const formattedDataFim = formatDateTime(dataFim);
+
+      const response = await axios.get('http://localhost:8080/transferencias', {
+        params: {
+          dataInicio: formattedDataInicio,
+          dataFim: formattedDataFim,
+          nomeOperador,
+        },
+      });
+      const data = response.data.transferencias;
+      const total = response.data.saldoTotal;
+      const totalNoPeriodo = response.data.saldoTotalNoPeriodo;
+      setTransferencias(data);
+      setSaldoTotal(total);
+      setSaldoTotalNoPeriodo(totalNoPeriodo);
+
+      console.log(response.data)
+
+    } catch (error) {
+      console.error('Erro ao buscar as transferências:', error);
+    }
+  };
+
+  const formatDateTime = (datetime: any) => {
+    const date = new Date(datetime);
+    const formattedDate = date.toISOString().split('T')[0];
+    const formattedTime = date.toTimeString().split(' ')[0];
+    return `${formattedDate}T${formattedTime}`;
+  };
+
   return (
     <Main>
       <Container>
         <Superior>
           <DataInicio>
             <Label>Data de Início:</Label>
-            <Input />
+            <Input type="date" value={dataInicio} onChange={(e) => setDataInicio(e.target.value)} />
           </DataInicio>
           <DataFim>
             <Label>Data de Fim:</Label>
-            <Input />
+            <Input type="date" value={dataFim} onChange={(e) => setDataFim(e.target.value)} />
           </DataFim>
           <NomeOperador>
             <Label>Nome do Operador Transacionado:</Label>
-            <Input />
+            <Input type="text" value={nomeOperador} onChange={(e) => setNomeOperador(e.target.value)} />
           </NomeOperador>
         </Superior>
         <Centro>
-          <ButtomPesquisar>Pesquisar</ButtomPesquisar>
+          <ButtomPesquisar onClick={handleSearch}>Pesquisar</ButtomPesquisar>
         </Centro>
 
         <TableContainer>
-          <Table>
-            <Caption> Saldo Total:    Saldo no Período:  </Caption>
-              <Thead>
-                <Thow>
-                  <TableHeader>Dados</TableHeader>
-                  <TableHeader>Valencia</TableHeader>
-                  <TableHeader>Tipo</TableHeader>
-                  <TableHeader>Nome do Operador Transacionado</TableHeader>
-                </Thow>
-              </Thead>
-              <Tbody>             
-
-                <Thow>
-                  <TableData>dataTransferencia</TableData>
-                  <TableData>valor</TableData>
-                  <TableData>tipo</TableData>
-                  <TableData>nomeOperadorTransacao</TableData>
-                </Thow>
-
-                <Thow>
-                  <TableData>dataTransferencia</TableData>
-                  <TableData>valor</TableData>
-                  <TableData>tipo</TableData>
-                  <TableData>nomeOperadorTransacao</TableData>
-                </Thow>
-
-                <Thow>
-                  <TableData>dataTransferencia</TableData>
-                  <TableData>valor</TableData>
-                  <TableData>tipo</TableData>
-                  <TableData>nomeOperadorTransacao</TableData>
-                </Thow>
-
-                <Thow>
-                  <TableData>dataTransferencia</TableData>
-                  <TableData>valor</TableData>
-                  <TableData>tipo</TableData>
-                  <TableData>nomeOperadorTransacao</TableData>
-                </Thow>
-
-              </Tbody>
-              <TableFooter>
-                <Thow>
-                  <TableData colSpan={4}>
-                    <PaginationButton>Anterior</PaginationButton>
-                    <PaginationButton>Próxima</PaginationButton>
-                  </TableData>
-                </Thow>
-              </TableFooter>
-          </Table>
+          <Caption>
+            <div>Saldo Total: <span>{saldoTotal}</span></div>
+            <div>Saldo no Período: <span>{saldoTotalNoPeriodo}</span></div>
+          </Caption>
+          <TableHeader>
+            <div>Dados</div>
+            <div>Valencia</div>
+            <div>Tipo</div>
+            <div>Nome do Operador Transacionado</div>
+          </TableHeader>
+          {displayedTransferencias.map((transferencia) => (
+            <TableData key={transferencia.id}>
+              <div>{transferencia.dataTransferencia}</div>
+              <div>{transferencia.valor}</div>
+              <div>{transferencia.tipo}</div>
+              <div>{transferencia.nomeOperadorTransacao}</div>
+            </TableData>
+          ))}
+          <TableFooter>
+            <StyledReactPaginate
+              previousLabel={'Anterior'}
+              nextLabel={'Próxima'}
+              breakLabel={'...'}
+              breakClassName={'break-me'}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={handlePageChange}
+              containerClassName={'pagination'}
+              activeClassName={'active'}
+            />
+          </TableFooter>
         </TableContainer>
       </Container>
     </Main>
--- src/app/style.ts
+++ src/app/style.ts
@@ -1,19 +1,91 @@
+import ReactPaginate from 'react-paginate';
 import styled from 'styled-components';
 
-export const Main = styled.main`
+export const TableContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  gap: 10px;
+`;
 
+export const Caption = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+
+  & div {
+    & span {
+      font-weight: 600;
+    }
+  }
+`;
+
+export const TableHeader = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  background-color: lightgray;
+  padding: 10px;
+
+  & div {
+    font-weight: 600;
+  }
+`;
+
+export const TableData = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 10px;
+
+  &:nth-child(even) {
+    background-color: #F5F5F5;
+  }
+`;
+
+export const TableFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgray;
+  padding: 10px;
+`;
+
+export const Main = styled.main`
+  display: block;
 `;
 
 export const Container = styled.div`
-
+  max-width: 1440px;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
 `;
 
 export const Superior = styled.div`
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 15%;
 `;
 
-export const Centro = styled.div`
+export const Label = styled.div`
+  font-weight: 600;
+`;
 
+export const LabelIn = styled.label`
+  font-weight: 500;
+  margin-left: 50px;
+`;
+
+export const Input = styled.input`
+  width: 300px;
+  border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  margin-top: 3px;
+  text-align: center;
+  padding: 5px;
+  font-size: 16px;
+  outline: none;
 `;
 
 export const DataInicio = styled.div`
@@ -21,60 +93,73 @@ export const DataInicio = styled.div`
 `;
 
 export const DataFim = styled.div`
-
-`;
-
-export const Label = styled.div`
-
-`;
-
-export const Input = styled.input`
-
+  
 `;
 
 export const NomeOperador = styled.div`
 
 `;
 
+export const Centro = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  height: 10%;
+`;
+
 export const ButtomPesquisar = styled.button`
+  width: 200px;
+  border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 10px;
+  cursor: pointer;
 
+  &:hover {
+    background-color: #C1C1C1;
+    font-weight: 600;
+  }
 `;
 
-export const TableContainer = styled.div`
-`;
+export const StyledReactPaginate = styled(ReactPaginate)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 
-export const Table = styled.table`
+  ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
 
-`;
+    li {
+      a {
+        padding: 8px 12px;
+        border-radius: 4px;
+        text-decoration: none;
+        color: #000;
+        background-color: #eaeaea;
+        cursor: pointer;
+        transition: background-color 0.2s, color 0.2s;
 
-export const Caption = styled.caption`
+        &.previous,
+        &.next {
+          background-color: #dcdcdc;
+        }
 
-`;
+        &.disabled {
+          cursor: not-allowed;
+          opacity: 0.5;
+        }
 
-export const Thead = styled.thead`
+        &.selected {
+          background-color: #000;
+          color: #fff;
+        }
 
-`;
-
-export const Thow = styled.tr`
-
-`;
-
-export const Tbody = styled.tbody`
-
-`;
-
-export const TableHeader = styled.th`
-
-`;
-
-export const TableData = styled.td`
-
-`;
-
-export const TableFooter = styled.tfoot`
-
-`;
-
-export const PaginationButton = styled.button`
-
+        &:hover {
+          background-color: #dcdcdc;
+        }
+      }
+    }
+  }
 `;
